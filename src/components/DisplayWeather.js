7de1a59c05aa2fd5e0a336de12bb851351a3c27e
Mehdih77 <mehdiwork77@gmail.com>
2021-10-10 11:36:31 +0330

    add project to github

---
+++ src/components/DisplayWeather.js
@@ -0,0 +1,129 @@
+import React from "react";
+import "./displayweather.css";
+
+function DisplayWeather({data}) {
+
+  console.log(data)
+
+  const iconurl = "http://openweathermap.org/img/w/" + `${!data.cod !== 404 ? data.weather[0].icon : null}` + ".png";
+
+  return (
+    <div className="displayweather">
+      {data.cod !== 404 ? (
+        <React.Fragment>
+          <div className="maincard">
+            <span className="cardtitle">
+              {data.name} , {data.sys.country}. Weather
+            </span>
+            <span className="cardsubtitle">
+              As of {new Date().toLocaleTimeString()}
+            </span>
+
+            <h1>
+              {" "}
+              {Math.floor(data.main.temp - 273.15)}
+              <sup>o</sup>
+            </h1>
+            <span className="weather-main">{data.weather[0].main}</span>
+            <img className="weather-icon" src={iconurl} alt=""/>
+            <span className="weather-description">
+              {" "}
+              {data.weather[0].description}
+            </span>
+          </div>
+          <div className="weatherdetails">
+            <div className="section1">
+              <table>
+                <tr>
+                  <td>
+                    <h4><i class="fas fa-temperature-high"></i>High/Low</h4>
+                  </td>
+                  <td>
+                    <span>
+                      {Math.floor(data.main.temp_max - 273.15)}/
+                      {Math.floor(data.main.temp_min - 273.15)}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4><i class="fas fa-water"></i>Humidity</h4>
+                  </td>
+                  <td>
+                    <span>{data.main.humidity} %</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4><i class="fas fa-poo-storm"></i>Pressure</h4>
+                  </td>
+                  <td>
+                    <span>{data.main.pressure} hPa</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4><i class="fas fa-low-vision"></i>Visibility</h4>
+                  </td>
+                  <td>
+                    <span>{data.visibility / 1000} Km</span>
+                  </td>
+                </tr>
+              </table>
+            </div>
+
+            <div className="section2">
+              <table>
+                <tr>
+                  <td>
+                    <h4><i class="fas fa-wind"></i>Wind</h4>
+                  </td>
+                  <td>
+                    <span>{Math.floor((data.wind.speed * 18) / 5)} km/hr</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4><i class="fas fa-wind"></i>Wind Direction</h4>
+                  </td>
+                  <td>
+                    <span>
+                      {data.wind.deg}
+                      <sup>o</sup> deg
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4><i class="fas fa-sun sunrise"></i>Sunrise</h4>
+                  </td>
+                  <td>
+                    <span>
+                      {new Date(data.sys.sunrise * 1000).toLocaleTimeString()}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4><i class="fas fa-sun"></i>Sunset</h4>
+                  </td>
+                  <td>
+                    <span>
+                      {new Date(data.sys.sunset * 1000).toLocaleTimeString()}
+                    </span>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </React.Fragment>
+      ) : (
+        <div className="maincard">
+          <h2>Error 404!</h2>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default DisplayWeather;
---
+++ src/components/Weather.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import DisplayWeather from "./DisplayWeather";
+import "./weather.css";
+
+function Weather() {
+  const [weather, setWeather] = useState({});
+
+  const [form, setForm] = useState({
+    city: "",
+    country: "",
+  });
+
+  const APIKEY = "bcda9a156c4a2107c473949d17b8dc15";
+
+  async function weatherData(e) {
+
+    e.preventDefault();
+
+    if (!form.city) {
+      alert("Add values");
+    } else {
+      await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&APPID=${APIKEY}`
+      )
+        .then( response => response.json())
+        .then( data => setWeather({data: data}));
+    }
+  }
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name] : e.target.value
+    })
+
+  };
+
+  return (
+    <div className="weather">
+      <span className="title">Weather App</span>
+      <br />
+      <form>
+        <input
+          type="text"
+          placeholder="city"
+          name="city"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Country"
+          name="country"
+          onChange={handleChange}
+        />
+        <button className="getweather" onClick={weatherData}>
+          Submit
+        </button>
+      </form>
+
+      {weather.data ? (
+        <div>
+          <DisplayWeather data={weather.data} />
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default Weather;
